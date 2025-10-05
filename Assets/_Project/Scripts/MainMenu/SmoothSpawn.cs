c104903496d4eda7d5dd51430147c9e887e83b2f
Pacelin <Warfacer2020@gmail.com>
2025-10-06 00:15:18 +0300

    feat: Loc and Main Menu fearture

---
+++ Assets/_Project/Scripts/MainMenu/SmoothSpawn.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Scripts.MainMenu
+{
+    public class SmoothSpawn : MonoBehaviour
+    {
+        [SerializeField] private float _lifetime = 60;
+
+        private bool _destroyed;
+        
+        private void OnEnable() => transform.DOScale(Random.Range(0.4f, 1.5f), 0.5f).From(0);
+        private void OnDisable() => transform.DOKill();
+
+        private void Update()
+        {
+            if (_destroyed) return;
+            _lifetime -= Time.deltaTime;
+            if (_lifetime <= 0)
+            {
+                _destroyed = true;
+                transform.DOScale(0, 0.5f)
+                    .OnComplete(() => Destroy(gameObject));
+            }
+        }
+    }
+}
