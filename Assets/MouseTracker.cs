155ca0dee9f754573403312f7b347b6fe0744392
vrusakov <vrusakov@gear-games.com>
2025-10-07 01:51:13 +0300

    фикс бабочек

--- Assets/MouseTracker.cs
+++ Assets/MouseTracker.cs
@@ -11,7 +11,13 @@ public class MouseTracker : MonoBehaviour
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = 0f;
-        Vector3 point = _camera.ScreenToWorldPoint(mousePosition);
+
+        Vector3 screenSize = new Vector3(Screen.width, Screen.height);
+
+        Vector3 point = new Vector3(mousePosition.x / screenSize.x, mousePosition.y / screenSize.y, 0f);
+        point.x = Mathf.Clamp01(point.x);
+        point.y = Mathf.Clamp01(point.y);
+
         _material.SetVector(MOUSE_POSITION_KEY, point);
     }
 }
