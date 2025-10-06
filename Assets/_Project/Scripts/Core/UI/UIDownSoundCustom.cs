4f531cedcc982712a99baefc0462deae9351f91c
Pacelin <Warfacer2020@gmail.com>
2025-10-06 23:07:04 +0300

    feat: some

---
+++ Assets/_Project/Scripts/Core/UI/UIDownSoundCustom.cs
@@ -0,0 +1,14 @@
+﻿using Scripts.Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Scripts.Core.UI
+{
+    public class UIDownSoundCustom : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField] private SoundEvent _event;
+        
+        public void OnPointerDown(PointerEventData eventData) =>
+            _event.PlayOneShot();
+    }
+}
---
+++ Assets/_Project/Scripts/Core/UI/UIHoverSoundCustom.cs
@@ -0,0 +1,13 @@
+﻿using Scripts.Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Scripts.Core.UI
+{
+    public class UIHoverSoundCustom : MonoBehaviour, IPointerEnterHandler
+    {
+        [SerializeField] private SoundEvent _event;
+        public void OnPointerEnter(PointerEventData eventData) =>
+            _event.PlayOneShot();
+    }
+}
